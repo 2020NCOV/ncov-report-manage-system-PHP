fafa35d044928b99cf0b10e07a3f8874af2d7f3c
xiami <2367386211@qq.com>
2020-04-02 22:19:20 +0800

    验证码

--- src/application/superadmin2020/controller/Login.php
+++ src/application/superadmin2020/controller/Login.php
@@ -11,18 +11,38 @@ class Login extends Controller{
     {
         //重置session
         Session::clear();
+        if(isset($_COOKIE['login_count'])){
+            $count=$_COOKIE['login_count'];
+        }
+        else{
+            $count=0;
+            cookie('login_count',0);
+        }
+        if($count>=3){
+            $this->assign('verify',true);
+        }
+        else {
+            $this->assign('verify',false);
+        }
+        $this->assign('count',$count);
         return  $this->fetch();
-    
     }
   
    
     public function dologin()
     {
-      
         $secure_code="沧海猎人";
         //重置session
+        $code = Request::instance()->post('code');
+        if(!isset($_COOKIE['login_count'])){
+            cookie('login_count',0);
+        }
+        if($_COOKIE['login_count']>=3){
+            if(!captcha_check($code)){
+                $this->error("验证码错误！",'login/index');
+            }
+        }
         Session::clear();
-        
         $username = Request::instance()->post('username');
         $password = Request::instance()->post('password');
         $md5password = md5($secure_code.md5($password));
@@ -50,9 +70,18 @@ class Login extends Controller{
             $this->redirect(url('superadmin2020/index/index'));
 
         }else{
+            if(!isset($_COOKIE['login_count'])){
+                cookie('login_count',0,1800);
+            }
+            $count=$_COOKIE['login_count'];
+            cookie('login_count',++$count,1800);
+            if($count>=3){
+                $this->assign('verify',true);
+            }
             $service = new \app\admin\service\Log();
             $service->write("1","登录失败".$username,$username);
-            $this->error("用户名或密码错误");
+            $this->error("用户名或密码错误",'login/index');
+            
         }
     }
 }
